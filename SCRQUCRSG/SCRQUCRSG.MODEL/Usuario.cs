7e2fde72f30823c3dba0f79e246563a3039e5451
Alejandro Baltodano <carlosreyesb99@gmail.com>
2017-03-07 14:16:44 -0600

    se configuro el model

---
+++ SCRQUCRSG/SCRQUCRSG.MODEL/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCRQUCRSG.MODEL
+{
+    public class Usuario
+    {
+
+        public int idUsuario = 0;
+        public String Cedula = String.Empty;
+        public String NombreUsuario = String.Empty;
+        public String NombreCompleto = String.Empty;
+        public int idROL = 0;
+
+    }
+}
