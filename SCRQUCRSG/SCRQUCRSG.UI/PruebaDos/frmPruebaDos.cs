890e2fe9a7a1a028af032d08dfd3820847aaf2ee
Alejandro Baltodano <carlosreyesb99@gmail.com>
2017-03-15 15:32:19 -0600

    prueba 8

---
+++ SCRQUCRSG/SCRQUCRSG.UI/PruebaDos/frmPruebaDos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SCRQUCRSG.UI.PruebaDos
+{
+    public partial class frmPruebaDos : Form
+    {
+        public frmPruebaDos()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("prueba 2");
+        }
+    }
+}
