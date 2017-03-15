067a85c16282adb72a8c91fb2a199ef6d0ff2b82
Jose Antonio Gomez <jant.gomez14@gmail.com>
2017-03-15 16:01:52 -0600

    prueba 15

--- SCRQUCRSG/SCRQUCRSG.UI/Prueba1/frmPrueba1.Designer.cs
+++ SCRQUCRSG/SCRQUCRSG.UI/Prueba1/frmPrueba1.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(102, 51);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 198);
+            this.button1.Size = new System.Drawing.Size(260, 237);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
