33a38012e9880b59a9878b3f1ff78ce34f1c9789
Alejandro Baltodano <carlosreyesb99@gmail.com>
2017-03-15 16:00:23 -0600

    prueba 14

--- SCRQUCRSG/SCRQUCRSG.UI/Prueba1/frmPrueba2.Designer.cs
+++ SCRQUCRSG/SCRQUCRSG.UI/Prueba1/frmPrueba2.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(77, 105);
+            this.button1.Location = new System.Drawing.Point(4, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(273, 122);
+            this.button1.Size = new System.Drawing.Size(395, 278);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
